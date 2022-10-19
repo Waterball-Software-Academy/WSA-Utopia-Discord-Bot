3ba4c9b51416c6f540e0e32859bd9ad628d5cd2d
johnny850807 <johnny850807@gmail.com>
2022-10-20 03:17:03 +0800

    :alembic: add an experiment implementation of ReactionAddEvent statistics function

--- src/main/kotlin/tw/waterballsa/alpha/wsabot/gamification/listeners/Statistics.kt
+++ src/main/kotlin/tw/waterballsa/alpha/wsabot/gamification/listeners/Statistics.kt
@@ -2,23 +2,26 @@ package tw.waterballsa.alpha.wsabot.gamification.listeners
 
 import dev.kord.core.event.message.ReactionAddEvent
 import dev.kord.core.event.message.ReactionRemoveAllEvent
-import dev.kord.core.event.message.ReactionRemoveEmojiEvent
 import me.jakejmattson.discordkt.dsl.listeners
+import me.jakejmattson.discordkt.extensions.fullName
 import mu.KotlinLogging
+import tw.waterballsa.alpha.wsabot.gamification.services.StatisticsService
 
-fun statisticsListener() = listeners {
+fun statisticsListener(service: StatisticsService) = listeners {
     val logger = KotlinLogging.logger {}
-    logger.trace("registered")
+    logger.trace { "registered" }
 
     on<ReactionAddEvent> {
-        logger.trace("notify")
-        val author = message.asMessage().author
+        logger.trace { "ReactionAddEvent" }
+        val author = message.asMessage().author!!
+        val count = service.incrementReaction(author)
+        val message = "${author.fullName} 目前總共獲得 $count 個表情。"
+        channel.createMessage(message)
+        val channelName = channel.asChannel().data.name.value
+        logger.info { "Messaging to '$channelName': \"$message\"" }
+    }
 
-    }
-    on<ReactionRemoveEmojiEvent> {
-        logger.trace("notify")
-    }
     on<ReactionRemoveAllEvent> {
-        logger.trace("notify")
+        logger.trace { "ReactionRemoveAllEvent" }
     }
 }
