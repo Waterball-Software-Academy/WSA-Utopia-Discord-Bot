9c51fd196fb50b0329a7878d9c4af58fda756afc
水球潘 WaterBall <johnny850807@gmail.com>
2023-03-21 19:45:37 +0800

    Release/1.2.1 (#45)

--- commons/src/main/kotlin/tw/waterballsa/utopia/commons/config/WsaDiscordProperties.kt
+++ commons/src/main/kotlin/tw/waterballsa/utopia/commons/config/WsaDiscordProperties.kt
@@ -2,10 +2,10 @@ package tw.waterballsa.utopia.commons.config
 
 import ch.qos.logback.core.util.OptionHelper.getEnv
 import me.jakejmattson.discordkt.annotations.Service
+import mu.KotlinLogging
 import tw.waterballsa.utopia.commons.utils.loadProperties
 
-private const val s = "DEPLOYMENT_ENV"
-
+val logger = KotlinLogging.logger {}
 @Service
 class WsaDiscordProperties() {
     var guildId: ULong
@@ -16,12 +16,14 @@ class WsaDiscordProperties() {
 
     init {
         val env = getEnv("DEPLOYMENT_ENV") ?: throw IllegalStateException("DEPLOYMENT_ENV environment variable is not set")
+        logger.info { "DEPLOYMENT_ENV=$env" }
         val properties = when (env) {
             "beta" -> loadProperties("wsa.beta.properties")
             "prod" -> loadProperties("wsa.prod.properties")
             else -> throw IllegalArgumentException("doesn't support the env name ${env}.")
         }
 
+        logger.info { properties }
         guildId = properties.getProperty("guild-id").toULong()
         unlockEntryMessageId = properties.getProperty("unlock-entry-message-id").toULong()
         selfIntroChannelId = properties.getProperty("self-intro-channel-id").toULong()
