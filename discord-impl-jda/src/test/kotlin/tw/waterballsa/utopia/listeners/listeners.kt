24737264e3cf1f4b96b95cbe8e6073c84de1f93b
johnny850807 <johnny850807@gmail.com>
2023-03-26 20:55:49 +0800

    fix test failure

--- discord-impl-jda/src/test/kotlin/tw/waterballsa/utopia/listeners/listeners.kt
+++ discord-impl-jda/src/test/kotlin/tw/waterballsa/utopia/listeners/listeners.kt
@@ -1,10 +1,9 @@
 package tw.waterballsa.utopia.listeners
 
-import net.dv8tion.jda.api.JDA
 import tw.waterballsa.utopia.commons.config.WsaDiscordProperties
 import tw.waterballsa.utopia.jda.listener
 
-internal fun testListener1(wsa: WsaDiscordProperties?, jda: JDA) = listener {}
-internal fun testListener2(wsa: WsaDiscordProperties?, jda: JDA) = listener {}
-internal fun testListener3(wsa: WsaDiscordProperties?, jda: JDA) = listener {}
+internal fun testListener1(wsa: WsaDiscordProperties?) = listener {}
+internal fun testListener2(wsa: WsaDiscordProperties?) = listener {}
+internal fun testListener3(wsa: WsaDiscordProperties?) = listener {}
 
