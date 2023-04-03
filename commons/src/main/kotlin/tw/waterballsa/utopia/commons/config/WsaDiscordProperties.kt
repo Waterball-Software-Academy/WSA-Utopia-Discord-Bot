358bf5121b618289f112cc70c50c2b8fc1a00574
akechetaki10736 <chi972121@gmail.com>
2023-04-03 12:11:17 +0800

    :hammer: 加入學院節目區（Prod）、線上派對區（Beta）的頻道 ID

--- commons/src/main/kotlin/tw/waterballsa/utopia/commons/config/WsaDiscordProperties.kt
+++ commons/src/main/kotlin/tw/waterballsa/utopia/commons/config/WsaDiscordProperties.kt
@@ -16,6 +16,7 @@ open class WsaDiscordProperties(properties: Properties) {
     val wsaCitizenRoleId: String
     val gentlemenForumCategoryId: String
     val wsaGentlemenBroadcastChannelId: String
+    val wasPartyChannelId: String
 
 
     init {
@@ -28,6 +29,7 @@ open class WsaDiscordProperties(properties: Properties) {
             wsaCitizenRoleId = getProperty("wsa-citizen-role-id")
             gentlemenForumCategoryId = getProperty("wsa-gentlemen-forum-category-id")
             wsaGentlemenBroadcastChannelId = getProperty("wsa-gentlemen-broadcast-channel-id")
+            wasPartyChannelId = getProperty("wsa-party-channel-id")
         }
     }
 }
