6b717b30eb3ddfa8a7ca81964567e94260b86695
hokou.shiau <hokou.shiau@gmail.com>
2023-04-17 22:02:21 +0800

    change /dice number to /random number change the describe

--- random/src/main/kotlin/tw/waterballsa/utopia/random/RandomDice.kt
+++ random/src/main/kotlin/tw/waterballsa/utopia/random/RandomDice.kt
@@ -6,21 +6,19 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData
 import tw.waterballsa.utopia.commons.config.WsaDiscordProperties
 import tw.waterballsa.utopia.jda.extensions.getOptionAsPositiveInt
-import tw.waterballsa.utopia.jda.extensions.getOptionAsStringWithLimitedLength
 import tw.waterballsa.utopia.jda.listener
 
 
 fun randomDice(wsa: WsaDiscordProperties) = listener {
     // channelId
-
     // [ /command ]
     // [ /<feature-module's name> <feature's name> <param1> <param2> <param3> ... ]
     // [ /random lottery number ]
     command {
-        Commands.slash("dice", "Get a dice number.")
+        Commands.slash("random", "Get a random number.")
             .addSubcommands(
-                SubcommandData("number", "get a dice number or input max value.")
-                    .addOption(OptionType.INTEGER, "number", "Number of a Max number.", false)
+                SubcommandData("number", "Get a random dice number or input a maximum value.")
+                    .addOption(OptionType.INTEGER, "number", "The value of maximum number.", false)
             )
     }
 
