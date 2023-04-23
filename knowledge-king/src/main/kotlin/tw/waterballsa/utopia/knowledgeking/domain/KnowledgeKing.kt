b66439070448cd4ac5d2b5ee9bd26c8b94ddc44f
wow890209 <63483663+wow890209@users.noreply.github.com>
2023-04-23 18:33:30 +0800

    Knowledge king kickoff (#7)
    * kickoff 5 acts
    
    * :construction: complete the first workable version
    
    ---------
    
    Co-authored-by: johnny850807 <johnny850807@gmail.com>

---
+++ knowledge-king/src/main/kotlin/tw/waterballsa/utopia/knowledgeking/domain/KnowledgeKing.kt
@@ -0,0 +1,130 @@
+package tw.waterballsa.utopia.knowledgeking.domain
+
+import java.lang.System.currentTimeMillis
+import java.util.*
+import kotlin.time.Duration.Companion.milliseconds
+
+class KnowledgeKing(private val quiz: Quiz, private val timeBetweenQuestionsInSeconds: Long) {
+    val id: String = UUID.randomUUID().toString()
+    private val questionIterator = quiz.questions.iterator()
+    var currentQuestion: Question? = null
+    private val scoreboard = Scoreboard()
+    private var gameStarted = false
+    private var gameOver = false
+    private var currentQuestionStartTimeInMillis: Long = 0
+
+    fun startContest(): List<Event> {
+        gameStarted = true
+        currentQuestion = nextQuestion()?.question
+
+        return listOf(ContestStartedEvent(quiz.questions.size),
+                NextQuestionEvent(1, this.currentQuestion!!, false))
+    }
+
+    fun answer(contestantId: String?, answer: Answer): AnsweredEvent {
+        if (gameOver) {
+            throw IllegalStateException("The game is over, cannot answer the question.")
+        }
+        return if (currentQuestion?.isCorrectAnswer(answer) == true) {
+            val secondsElapsed = (currentTimeMillis() - currentQuestionStartTimeInMillis).milliseconds.inWholeSeconds
+            scoreboard.win(contestantId!!, 500  * secondsElapsed / timeBetweenQuestionsInSeconds)
+            AnsweredEvent(answer, contestantId, AnswerResult.CORRECT)
+        } else {
+            AnsweredEvent(answer, contestantId!!, AnswerResult.WRONG)
+        }
+    }
+
+    fun nextQuestion(): NextQuestionEvent? {
+        if (gameOver || !gameStarted || !questionIterator.hasNext()) {
+            return null
+        }
+        currentQuestionStartTimeInMillis = currentTimeMillis()
+        currentQuestion = questionIterator.next()
+        val nextQuestion = currentQuestion!!
+        val isLastQuestion = !questionIterator.hasNext()
+        return NextQuestionEvent(nextQuestion.number, nextQuestion, isLastQuestion)
+    }
+
+    fun endGame(): Ranking {
+        gameOver = true
+        return scoreboard.ranking()
+    }
+
+    fun isGameOver(): Boolean {
+        return gameOver
+    }
+}
+
+class Scoreboard {
+    private val board = mutableMapOf<String, Long>()
+
+    fun win(contestantId: String, score: Long) {
+        board[contestantId] = board[contestantId] ?: (0 + score)
+    }
+
+    fun ranking(): Ranking {
+        val sortedBoard = board.entries.sortedByDescending { it.value }
+        val ranks = sortedBoard.mapIndexed { index, entry -> Rank(index + 1, entry.key, entry.value) }
+        return Ranking(ranks)
+    }
+}
+
+class Ranking(val ranks: List<Rank>) {
+    fun rank(rankNumber: Int): Rank {
+        if (rankNumber > ranks.size) {
+            throw IllegalArgumentException("Cannot find the rank (by number $rankNumber).")
+        }
+        return ranks[rankNumber]
+    }
+}
+
+data class Rank(val rankNumber: Int, val contestantId: String, var score: Long)
+
+
+data class Quiz(val topic: String, val questions: List<Question>) {}
+
+data class Question(val number: Int, val description: String, val options: List<String>,
+                    val type: QuestionType, val answer: AnswerSpec,
+                    val explanation: String? = null) {
+    fun isCorrectAnswer(answer: Answer): Boolean {
+        return when (type) {
+            QuestionType.SINGLE -> {
+                val singleAnswerSpec = this.answer as? SingleAnswerSpec
+                singleAnswerSpec?.optionNumber == (answer as SingleChoiceAnswer).optionNumber
+            }
+
+            QuestionType.MULTIPLE -> {
+                val multipleAnswerSpec = this.answer as? MultipleAnswerSpec
+                multipleAnswerSpec?.optionNumbers?.toSet() == (answer as MultipleChoicesAnswer).optionNumbers.toSet()
+            }
+        }
+    }
+
+    enum class QuestionType {
+        SINGLE,
+        MULTIPLE
+    }
+}
+
+abstract class AnswerSpec()
+data class SingleAnswerSpec(val optionNumber: Int) : AnswerSpec() {
+
+}
+
+data class MultipleAnswerSpec(val optionNumbers: List<Int>) : AnswerSpec() {
+}
+
+abstract class Answer(open val timestamp: Date) {
+
+}
+
+data class SingleChoiceAnswer(val optionNumber: Int, override val timestamp: Date = Date()) : Answer(timestamp) {
+
+}
+
+data class MultipleChoicesAnswer(val optionNumbers: List<Int>, override val timestamp: Date = Date()) : Answer(timestamp) {
+}
+
+enum class AnswerResult {
+    WRONG, CORRECT
+}
--- knowledge-king/src/main/kotlin/tw/waterballsa/utopia/knowledgeking/listeners.kt
+++ knowledge-king/src/main/kotlin/tw/waterballsa/utopia/knowledgeking/listeners.kt
@@ -1,43 +1,200 @@
 package tw.waterballsa.utopia.knowledgeking
-//
-//import dev.kord.common.entity.Snowflake
-//import dev.kord.core.behavior.channel.MessageChannelBehavior
-//import dev.kord.core.event.gateway.ReadyEvent
-//import kotlinx.coroutines.delay
-//import me.jakejmattson.discordkt.dsl.listeners
-//
-//var game : Game? = null
-//fun Listener() = listeners {
-//
-//    on<ReadyEvent> {
-//        val c = MessageChannelBehavior(Snowflake(1038657903437037590),discord.kord).asChannel();
-//        c.createMessage("知識王已啟動")
-//
-//        scheduleTaskAtEightPM {
-//
-//            c.createMessage("知識王比賽10分鐘後開始囉")
-//            delay(300000)
-//            c.createMessage("知識王比賽5分鐘後開始囉")
-//            delay(300000)
-//
-//            game = Game(c)
-//
-//            game!!.CreateRanking()
-//            val c = game!!.CreateMenu(discord)
-//            c.createMessage("知識王比賽已準備好 10秒後開始答題")
-//            delay(10000)
-//
-//            while (!game!!.IsEndGame()) {
-//                c.createMessage("開始下一題")
-//                game!!.Next()
-//                delay(10500)
-//                c.createMessage("答題結束 等待兩秒開始")
-//                game!!.UpdateRanking()
-//                delay(2000)
-//            }
-//            c.createMessage("遊戲結束!!")
-//        }
-//    }
-//
-//
-//}
+
+import dev.minn.jda.ktx.messages.Embed
+import mu.KotlinLogging
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
+import net.dv8tion.jda.api.interactions.components.buttons.Button
+import tw.waterballsa.utopia.commons.config.WsaDiscordProperties
+import tw.waterballsa.utopia.commons.extensions.dailyScheduling
+import tw.waterballsa.utopia.commons.extensions.scheduleDelay
+import tw.waterballsa.utopia.jda.listener
+import tw.waterballsa.utopia.knowledgeking.domain.*
+import java.util.*
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+
+private val log = KotlinLogging.logger {}
+private val timer = Timer()
+
+// Sets the announcement time for the upcoming Knowledge King game to 10:20 PM
+private val announcementTime = Calendar.getInstance().apply {
+    set(Calendar.HOUR_OF_DAY, 22)
+    set(Calendar.MINUTE, 20)
+    set(Calendar.SECOND, 0)
+}!!
+
+// Specifies the duration of time given to each contestant to prepare before the start of the game
+// 10.minutes.inWholeMilliseconds
+private val prepareDurationInMillis = 5.seconds.inWholeMilliseconds
+
+private const val numberOfQuestions = 5
+private const val timeBetweenQuestionsInSeconds = 15L
+private const val timeBetweenAnswerRevealedAndNextQuestionInSeconds = 8L
+
+var knowledgeKing: KnowledgeKing? = null
+
+fun knowledgeKing(wsa: WsaDiscordProperties, jda: JDA, chatGptQuestionParser: ChatGptQuestionParser) = listener {
+    launchKnowledgeKingScheduling(wsa, jda, chatGptQuestionParser)
+
+    on<ButtonInteractionEvent> {
+        deferReply(true).queue()
+        if (knowledgeKing != null && !knowledgeKing!!.isGameOver()) {
+            val question = knowledgeKing!!.currentQuestion!!
+            for (optionNumber in 0..question.options.size) {
+                val buttonId = buttonId(question.number, optionNumber)
+                if (buttonId == button.id) {
+                    val contestantId = member?.id
+                    val answer: Char = 'A' + optionNumber
+                    log.info { "[Answered] {\"contestantId\": \"$contestantId\", \"answer\": \"$answer\"}" }
+                    knowledgeKing!!.answer(contestantId, SingleChoiceAnswer(optionNumber))
+                    hook.editOriginal("已經接受到你的答案。").queue()
+                }
+            }
+        }
+    }
+}
+
+private fun launchKnowledgeKingScheduling(wsa: WsaDiscordProperties, jda: JDA, chatGpt: ChatGptQuestionParser) {
+    timer.dailyScheduling(announcementTime) {
+        val knowledgeKingChannel = jda.getTextChannelById(wsa.knowledgeKingChannelId)
+        val topic = generateTopic()
+        announceTopic(topic, knowledgeKingChannel!!)
+        val quiz = generateQuizForTopic(topic, chatGpt)
+        scheduleFirstQuestion(quiz, wsa, jda, knowledgeKingChannel)
+    }
+}
+
+private fun scheduleFirstQuestion(quiz: Quiz, wsa: WsaDiscordProperties, jda: JDA, knowledgeKingChannel: TextChannel) {
+    timer.scheduleDelay(prepareDurationInMillis) {
+        knowledgeKing = KnowledgeKing(quiz, timeBetweenQuestionsInSeconds)
+        val events = knowledgeKing!!.startContest()
+        handleEvents(events, wsa, jda)
+        scheduleRevealAnswer(events.filterIsInstance<NextQuestionEvent>().first(),
+                knowledgeKingChannel)
+    }
+}
+
+private fun scheduleRevealAnswer(nextQuestionEvent: NextQuestionEvent, knowledgeKingChannel: TextChannel) {
+    log.info { "[Revealing next question] \"{\"delayInSeconds\": $timeBetweenAnswerRevealedAndNextQuestionInSeconds}\"" }
+    timer.scheduleDelay(timeBetweenQuestionsInSeconds.seconds.inWholeMilliseconds) {
+        revealTheAnswer(nextQuestionEvent, knowledgeKingChannel)
+        if (nextQuestionEvent.isLastQuestion) {
+            val rank = knowledgeKing!!.endGame()
+            revealRank(rank, knowledgeKingChannel)
+        } else {
+            scheduleNextQuestion(knowledgeKingChannel)
+        }
+    }
+}
+
+private fun scheduleNextQuestion(knowledgeKingChannel: TextChannel) {
+    log.info { "[Starting next question] {\"number\": ${knowledgeKing!!.currentQuestion!!.number + 1}, \"delayInSeconds\": $timeBetweenAnswerRevealedAndNextQuestionInSeconds} }\n        " }
+    timer.scheduleDelay(timeBetweenAnswerRevealedAndNextQuestionInSeconds.seconds.inWholeMilliseconds) {
+        val nextQuestionEvent = knowledgeKing!!.nextQuestion()!!
+        handleNextQuestionEvent(nextQuestionEvent, knowledgeKingChannel)
+        scheduleRevealAnswer(nextQuestionEvent, knowledgeKingChannel)
+    }
+}
+
+private fun revealRank(ranking: Ranking, knowledgeKingChannel: TextChannel) {
+    val rankings = ranking.ranks.stream().limit(5)
+        .map { " 第 ${it.rankNumber} 名： <@${it.contestantId}> - ${it.score} 分" }
+        .toList()
+    knowledgeKingChannel.sendMessage(
+        "本次【全民軟體知識王】活動結束\n感謝大家的參與\n本次的排名如下：\n ${
+            rankings.joinToString("\n")
+        }"
+    ).queue()
+}
+
+private fun revealTheAnswer(nextQuestionEvent: NextQuestionEvent, knowledgeKingChannel: TextChannel) {
+    // Check answer spec type
+    val answerOption: String = when (val answer = nextQuestionEvent.question.answer) {
+        is SingleAnswerSpec -> "${'A' + answer.optionNumber}"
+
+        is MultipleAnswerSpec -> answer.optionNumbers.map {
+            'A' + it
+        }.joinToString(", ")
+
+        else -> throw IllegalStateException("Doesn't support the answer type ${answer.javaClass.simpleName}.")
+    }
+
+    // Answer message template
+    val answerMessage = """
+        正確解答: $answerOption 
+    """.trimIndent()
+
+    // Send message to text channel
+    log.info { "[Reveal Answer] {\"answerMessage\": \"${answerMessage}\"}" }
+    knowledgeKingChannel.sendMessage(answerMessage).queue()
+}
+
+
+private fun generateQuizForTopic(topic: String, chatGpt: ChatGptQuestionParser): Quiz {
+    val questions = chatGpt.generateQuestions(topic, numberOfQuestions)
+    return Quiz(topic, questions)
+}
+
+private fun announceTopic(topic: String, knowledgeKingChannel: TextChannel) {
+    knowledgeKingChannel.sendMessage("""
+                【全民軟體知識王】
+                ${prepareDurationInMillis.minutes} 分鐘後，比賽開始啊！快來玩！
+                比賽主題：$topic
+            """.trimIndent()).queue()
+}
+
+private fun generateTopic(): String {
+    return "Computer Science" // only support for CS in the current version
+}
+
+private fun handleEvents(events: List<Event>, wsa: WsaDiscordProperties, jda: JDA) {
+    val knowledgeKingChannel = jda.getTextChannelById(wsa.knowledgeKingChannelId)!!
+    events.forEach { event ->
+        when (event) {
+            is NextQuestionEvent -> handleNextQuestionEvent(event, knowledgeKingChannel)
+        }
+    }
+}
+
+
+private fun handleNextQuestionEvent(event: NextQuestionEvent, knowledgeKingChannel: TextChannel) {
+    log.info { "[Next Question] {\"number\": ${event.questionNumber}, \"question\":\"${event.question.description}\"" }
+
+    val question = event.question
+
+    knowledgeKingChannel.sendMessageEmbeds(Embed {
+        this.title = "第 ${question.number} 題"
+        this.description = "${question.description}\n${
+            question.options.mapIndexed { i, option ->
+                "${'A' + i}) $option"
+            }.joinToString("\n")
+        }"
+    }).addActionRow(
+            Button.primary(buttonId(question.number, 0), "A"),
+            Button.primary(buttonId(question.number, 1), "B"),
+            Button.primary(buttonId(question.number, 2), "C"),
+            Button.primary(buttonId(question.number, 3), "D")
+    ).queue {
+        log.info { "[Question Posted] \"question type\":\"${question.type}\",\"question number\" : ${question.number}" }
+    }
+}
+
+private fun buttonId(questionNumber: Int, optionNumber: Int): String {
+    return "${knowledgeKing!!.id}-$questionNumber-$optionNumber"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
