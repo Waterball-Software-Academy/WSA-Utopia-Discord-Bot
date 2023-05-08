6b6002a2702a21e5b893d1a1ae9dbeaed94f5d61
Jack Huang <w305jack@gmail.com>
2023-05-08 16:38:27 +0800

    feat: add jtokkit for calc tokens

---
+++ chatgpt-api/src/main/kotlin/tw/waterballsa/utopia/chatgpt/JTokkit.kt
@@ -0,0 +1,19 @@
+package tw.waterballsa.utopia.chatgpt
+
+import com.knuddels.jtokkit.Encodings
+import com.knuddels.jtokkit.api.Encoding
+import com.knuddels.jtokkit.api.EncodingType
+
+/**
+ * use with OpenAI's GPT-3.5 models.
+ */
+class JTokkit {
+    private val registry = Encodings.newDefaultEncodingRegistry()
+    private val encodingType: EncodingType = EncodingType.CL100K_BASE
+
+    fun size(string: String): Int {
+        val encoding: Encoding = registry.getEncoding(encodingType)
+        val encoded: List<Int> = encoding.encode(string)
+        return encoded.size
+    }
+}
