14aa621fd3dc8f1e5f8eb173ca556b25eb676add
Jack Huang <w305jack@gmail.com>
2023-05-09 22:23:09 +0800

    style: reformat code

--- chatgpt-api/src/main/kotlin/tw/waterballsa/utopia/chatgpt/ChatGptApi.kt
+++ chatgpt-api/src/main/kotlin/tw/waterballsa/utopia/chatgpt/ChatGptApi.kt
@@ -17,9 +17,10 @@ val log = KotlinLogging.logger {}
 @Named
 class ChatGptAPI {
     private val _mapper = ObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     private val endpoint = "https://api.openai.com/v1/chat/completions"
     private val _model = "gpt-3.5-turbo"
+
     // this is default property of completion tokens
     private val _maxTokens = 2000
     private val _temperature = 0.8
@@ -45,7 +46,7 @@ class ChatGptAPI {
     fun chat(messages: Array<Message>, maxTokens: Int = _maxTokens): Response {
         log.info { "[Sending Chat] {\"totalContentLength\": ${messages.sumOf { m -> m.content.length }}}" }
         val requestBody = _mapper.writeValueAsString(
-                Request(1, _model, messages, maxTokens, _temperature)
+            Request(1, _model, messages, maxTokens, _temperature)
         )
         val httpRequest = chatApiRequest(requestBody)
         val httpResponse = sendRequest(httpRequest)
@@ -62,59 +63,59 @@ class ChatGptAPI {
     }
 
     private fun chatApiRequest(requestBody: String): HttpRequest = HttpRequest.newBuilder()
-            .uri(URI(endpoint))
-            .POST(BodyPublishers.ofString(requestBody))
-            .header("Authorization", String.format("Bearer %s", secret))
-            .header("Content-Type", "application/json")
-            .build()
+        .uri(URI(endpoint))
+        .POST(BodyPublishers.ofString(requestBody))
+        .header("Authorization", String.format("Bearer %s", secret))
+        .header("Content-Type", "application/json")
+        .build()
 
     private fun sendRequest(request: HttpRequest): HttpResponse<String> = HttpClient.newBuilder()
-            .build()
-            .send(request, HttpResponse.BodyHandlers.ofString())
+        .build()
+        .send(request, HttpResponse.BodyHandlers.ofString())
 
     class Request(
-            val n: Int, // How many chat completion choices to generate for each input message.
-            val model: String,
-            val messages: Array<Message>,
-            val max_tokens: Int,
-            val temperature: Double
+        val n: Int, // How many chat completion choices to generate for each input message.
+        val model: String,
+        val messages: Array<Message>,
+        val max_tokens: Int,
+        val temperature: Double
     )
 
     class Response(
-            val id: String = "",
-            val `object`: String = "",
-            val created: Long = 0,
-            val model: String = "",
-            val usage: Usage = Usage(),
-            val choices: List<Choice> = emptyList(),
-            val error: Error = Error()
+        val id: String = "",
+        val `object`: String = "",
+        val created: Long = 0,
+        val model: String = "",
+        val usage: Usage = Usage(),
+        val choices: List<Choice> = emptyList(),
+        val error: Error = Error()
     ) {
         fun firstMessageContent() = choices[0].message.content
     }
 
     class Error(
-            val message: String = "",
-            val type: String = "",
-            val param: Long = 0,
-            val code: String = ""
+        val message: String = "",
+        val type: String = "",
+        val param: Long = 0,
+        val code: String = ""
     )
 
 
     class Usage(
-            val prompt_tokens: Int = 0,
-            val completion_tokens: Int = 0,
-            val total_tokens: Int = 0
+        val prompt_tokens: Int = 0,
+        val completion_tokens: Int = 0,
+        val total_tokens: Int = 0
     )
 
     class Choice(
-            val message: Message = Message(),
-            val finish_reason: String = "",
-            val index: Int = 0
+        val message: Message = Message(),
+        val finish_reason: String = "",
+        val index: Int = 0
     )
 
     class Message(
-            val role: String = "",
-            val content: String = ""
+        val role: String = "",
+        val content: String = ""
     )
 
 }
