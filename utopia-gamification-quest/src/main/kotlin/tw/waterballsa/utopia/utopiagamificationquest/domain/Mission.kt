92a4dfe183100512499723026d9336ddd92b03f6
wow890209 <63483663+wow890209@users.noreply.github.com>
2023-06-05 22:01:18 +0800

    create utopia-gamification-quest feature (#126)

---
+++ utopia-gamification-quest/src/main/kotlin/tw/waterballsa/utopia/utopiagamificationquest/domain/Mission.kt
@@ -0,0 +1,7 @@
+package tw.waterballsa.utopia.utopiagamificationquest.domain
+
+//TODO infinite會先完成這部分和具體行動相關的
+class Mission(val player: Player, val quest: PublicQuest) {
+
+
+}
---
+++ utopia-gamification-quest/src/main/kotlin/tw/waterballsa/utopia/utopiagamificationquest/domain/PublicQuest.kt
@@ -0,0 +1,5 @@
+package tw.waterballsa.utopia.utopiagamificationquest.domain
+
+//TODO Zaza完成這個類別
+class PublicQuest {
+}
