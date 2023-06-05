e276efe680ea2776e3e3ce10c585239a1951a050
wow890209 <63483663+wow890209@users.noreply.github.com>
2023-06-05 20:16:07 +0800

    create utopia gamification quest feature (#125)

---
+++ utopia-gamification-quest/src/main/kotlin/tw/waterballsa/utopia/utopiagamificationquest/UtopiaGamificationQuestListener.kt
@@ -0,0 +1,34 @@
+package tw.waterballsa.utopia.utopiagamificationquest
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.build.CommandData
+import net.dv8tion.jda.api.interactions.commands.build.Commands
+import org.springframework.stereotype.Component
+import tw.waterballsa.utopia.jda.UtopiaListener
+
+const val UTOPIA_COMMAND_NAME = "utopia"
+
+@Component
+class UtopiaGamificationQuestListener : UtopiaListener() {
+
+    override fun commands(): List<CommandData> {
+        return listOf(
+            Commands.slash("utopia", "utopia command")
+        )
+    }
+
+    override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+        with(event) {
+            if (!isValidCommand()) {
+                return
+            }
+            
+
+        }
+    }
+
+    private fun SlashCommandInteractionEvent.isValidCommand(): Boolean {
+        return fullCommandName == UTOPIA_COMMAND_NAME
+    }
+
+}
