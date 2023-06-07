cc6d54219e678498dc3f2d2f91d47f74d61ce3a9
Mirai1129 <minecraftmiku831@gmail.com>
2023-06-07 21:12:39 +0800

    Implement Player Model

--- gamification/src/main/kotlin/tw.waterballsa.utopia.gamification/commands/Gamification.kt
+++ gamification/src/main/kotlin/tw.waterballsa.utopia.gamification/commands/Gamification.kt
@@ -1,11 +1,27 @@
 package tw.waterballsa.utopia.gamification.commands
-//
-//import me.jakejmattson.discordkt.commands.commands
-//
-//fun gamification() = commands("gamification") {
-//    slash("gamification", "A 'gamification' command.") {
-//        execute {
-//            respond("Start gamification!")
-//        }
-//    }
-//}
+
+import org.slf4j.event.Level
+import java.lang.IllegalArgumentException
+
+class Player(val id: Snowflake, var name: String, var exp: Long, level: Int) {
+    var level: Int
+        set(value) {
+            if (value >= 1) {
+                field = value
+            } else {
+                throw IllegalArgumentException("your level must more than 1")
+            }
+        }
+
+    init {
+        this.level = level
+    }
+
+    fun gainExp(exp: Long) {
+        this.exp += exp
+    }
+
+    fun levelUp() {
+        level += 1
+    }
+}
