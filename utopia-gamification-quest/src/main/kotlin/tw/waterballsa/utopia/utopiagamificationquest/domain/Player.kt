040993725e2a2971b11f182dfe4626a716ffb929
Mirai1129 <minecraftmiku831@gmail.com>
2023-06-09 21:19:10 +0800

    Implement Player Model

--- utopia-gamification-quest/src/main/kotlin/tw/waterballsa/utopia/utopiagamificationquest/domain/Player.kt
+++ utopia-gamification-quest/src/main/kotlin/tw/waterballsa/utopia/utopiagamificationquest/domain/Player.kt
@@ -1,5 +1,27 @@
 package tw.waterballsa.utopia.utopiagamificationquest.domain
 
-//TODO 恐龍在這裡完成你的任務
-class Player {
+import org.slf4j.event.Level
+import java.lang.IllegalArgumentException
+
+class Player(val id: String, var name: String, var exp: Long, level: Int) {
+    var level: Int = 0
+        set(value) {
+            if (value >= 1) {
+                field = value
+            } else {
+                throw IllegalArgumentException("your level must more than 1")
+            }
+        }
+
+    init {
+        this.level = level
+    }
+
+    fun gainExp(exp: Long) {
+        this.exp += exp
+    }
+
+    fun levelUp() {
+        level += 1
+    }
 }
