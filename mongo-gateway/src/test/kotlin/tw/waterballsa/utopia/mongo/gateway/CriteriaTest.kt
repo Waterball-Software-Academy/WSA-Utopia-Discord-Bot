efd0715c8ade886be51e77e1bad8aa96363f8410
timm <tim.tf.chen@gamil.com>
2023-07-21 00:04:17 +0800

    adjust test code format

--- mongo-gateway/src/test/kotlin/tw/waterballsa/utopia/mongo/gateway/CriteriaTest.kt
+++ mongo-gateway/src/test/kotlin/tw/waterballsa/utopia/mongo/gateway/CriteriaTest.kt
@@ -1,7 +1,6 @@
 package tw.waterballsa.utopia.mongo.gateway
 
 import net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson
-import org.assertj.core.api.Assertions.assertThat
 import org.bson.Document
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -11,8 +10,8 @@ internal class CriteriaTest {
 
     @Test
     fun emptyCriteria() {
-        assertThat(Criteria().getCriteriaObject())
-                .isEqualTo(Document())
+        assertThatJson(Criteria().getCriteriaObject())
+                .isEqualTo("{}")
     }
 
     @Test
@@ -60,8 +59,159 @@ internal class CriteriaTest {
                   }
                 }
                 """
-        assertThat(Criteria("name").`is`("Tom")
+        assertThatJson(Criteria("name").`is`("Tom")
                 .and("age").ne(18).getCriteriaObject())
                 .isEqualTo(Document(mapOf("name" to "Tom", "age" to Document(mapOf("\$ne" to 18)))))
+    }
+
+    @Test
+    fun ageGreaterThan20() {
+        val expectedValue =
+                """
+                {
+                  "age": {
+                    "${'$'}gt": 20
+                  }
+                }
+                """
+        assertThatJson(Criteria("age").gt(20).getCriteriaObject())
+                .isEqualTo(expectedValue)
+    }
+
+    @Test
+    fun ageGreaterThanAndEqual20() {
+        val expectedValue =
+                """
+                {
+                  "age": {
+                    "${'$'}gte": 20
+                  }
+                }
+                """
+        assertThatJson(Criteria("age").gte(20).getCriteriaObject())
+                .isEqualTo(expectedValue)
+    }
+
+    @Test
+    fun ageLessThan50() {
+        val expectedValue =
+                """
+                {
+                  "age": {
+                    "${'$'}lt": 50
+                  }
+                }
+                """
+        assertThatJson(Criteria("age").lt(50).getCriteriaObject())
+                .isEqualTo(expectedValue)
+    }
+
+    @Test
+    fun ageLessThanAndEqual50() {
+        val expectedValue =
+                """
+                {
+                  "age": {
+                    "${'$'}lte": 50
+                  }
+                }
+                """
+        assertThatJson(Criteria("age").lte(50).getCriteriaObject())
+                .isEqualTo(expectedValue)
+    }
+
+    @Test
+    fun nameInTomAndMaryWithVararg() {
+        val expectedValue =
+                """
+                {
+                  "name": {
+                    "${'$'}in": ["Tom","Mary"]
+                  }
+                }
+                """
+        assertThatJson(Criteria("name").`in`("Tom", "Mary").getCriteriaObject())
+                .isEqualTo(expectedValue)
+    }
+
+    @Test
+    fun nameInTomAndMaryWithList() {
+        val expectedValue =
+                """
+                {
+                  "name": {
+                    "${'$'}in": ["Tom","Mary"]
+                  }
+                }
+                """
+        assertThatJson(Criteria("name").`in`(listOf("Tom", "Mary")).getCriteriaObject())
+                .isEqualTo(expectedValue)
+    }
+
+    @Test
+    fun nameNotInTomAndMaryWithVararg() {
+        val expectedValue =
+                """
+                {
+                  "name": {
+                    "${'$'}nin": ["Tom","Mary"]
+                  }
+                }
+                """
+        assertThatJson(Criteria("name").nin("Tom", "Mary").getCriteriaObject())
+                .isEqualTo(expectedValue)
+    }
+
+    @Test
+    fun nameNotInTomAndMaryWithList() {
+        val expectedValue =
+                """
+                {
+                  "name": {
+                    "${'$'}nin": ["Tom","Mary"]
+                  }
+                }
+                """
+        assertThatJson(Criteria("name").nin(listOf("Tom", "Mary")).getCriteriaObject())
+                .isEqualTo(expectedValue)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["true,true", "false,false"])
+    fun nameExistOrNot(existValue: Boolean, expectedExistValue: Boolean) {
+        val expectedValue =
+                """
+                {
+                  "name": {
+                    "${'$'}exists": $expectedExistValue
+                  }
+                }
+                """
+        assertThatJson(Criteria("name").exists(existValue).getCriteriaObject())
+                .isEqualTo(expectedValue)
+    }
+
+    @Test
+    fun nameIsTomOrNameIsMaryAndAgeGreaterThan18() {
+        val expectedValue =
+                """
+                {
+                  "${'$'}or": [
+                    {
+                      "name": "Tom"
+                    },
+                    {
+                      "name": "Mary",
+                      "age": {
+                        "${'$'}gt": 18
+                      }
+                    }
+                  ]
+                }    
+                """
+        assertThatJson(Criteria().orOperator(
+                Criteria("name").`is`("Tom"),
+                Criteria("name").`is`("Mary").and("age").gt(18)).getCriteriaObject())
+                .isEqualTo(expectedValue)
     }
 }
