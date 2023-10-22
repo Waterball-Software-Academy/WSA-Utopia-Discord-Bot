3d98abe1fcab46dd3e60da38a144f0f77eb7868c
wally5077 <wally55077@gmail.com>
2023-10-22 23:47:59 +0800

    :bug: Fix Mongo test kit connection failure

--- mongo-gateway-impl/src/main/kotlin/tw/waterballsa/utopia/mongo/gatweay/config/MongoDBConfiguration.kt
+++ mongo-gateway-impl/src/main/kotlin/tw/waterballsa/utopia/mongo/gatweay/config/MongoDBConfiguration.kt
@@ -1,14 +1,17 @@
 package tw.waterballsa.utopia.mongo.gatweay.config
 
-import ch.qos.logback.core.util.OptionHelper
-import com.fasterxml.jackson.databind.DeserializationFeature
+import ch.qos.logback.core.util.OptionHelper.getEnv
+import ch.qos.logback.core.util.OptionHelper.getSystemProperty
+import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClients
+import org.reflections.Reflections
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.beans.factory.support.AbstractBeanDefinition
@@ -36,28 +39,29 @@ import kotlin.reflect.full.findAnnotation
 
 @Configuration
 open class MongoDBConfiguration {
-
     companion object {
+        private const val MONGO_CONNECTION_URI_KEY = "MONGO_CONNECTION_URI"
+
         internal val MAPPER = ObjectMapper()
-            .registerKotlinModule()
-            .registerModule(JavaTimeModule())
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .registerKotlinModule()
+                .registerModule(JavaTimeModule())
+                .disable(WRITE_DATES_AS_TIMESTAMPS)
+                .configure(FAIL_ON_UNKNOWN_PROPERTIES, false)
     }
 
     @Bean
     open fun mongoTemplate(context: ApplicationContext): MongoTemplate {
-        val uri = OptionHelper.getEnv("MONGO_CONNECTION_URI")?.trim()
-            ?: "mongodb://localhost:28017"
+        val uri = getEnv(MONGO_CONNECTION_URI_KEY)
+                ?: getSystemProperty(MONGO_CONNECTION_URI_KEY, "mongodb://localhost:28017")
         val settings = MongoClientSettings.builder()
-            .applyConnectionString(ConnectionString(uri))
-            .applyToConnectionPoolSettings { builder ->
-                builder.maxSize(10)
-            }
-            .build()
+                .applyConnectionString(ConnectionString(uri.trim()))
+                .applyToConnectionPoolSettings { builder ->
+                    builder.maxSize(10)
+                }
+                .build()
         val wsaDiscordProperties = context.getBean(WsaDiscordProperties::class.java)
         val factory =
-            SimpleMongoClientDatabaseFactory(MongoClients.create(settings), wsaDiscordProperties.mongoDatabase)
+                SimpleMongoClientDatabaseFactory(MongoClients.create(settings), wsaDiscordProperties.mongoDatabase)
 
         // remove _class field
         val converter = MappingMongoConverter(DefaultDbRefResolver(factory), MongoMappingContext())
@@ -67,24 +71,25 @@ open class MongoDBConfiguration {
     }
 }
 
-
 @Component
 open class MyBeanFactoryPostProcessor : BeanFactoryPostProcessor, ApplicationContextAware {
 
     private lateinit var applicationContext: ApplicationContext
 
+    override fun setApplicationContext(applicationContext: ApplicationContext) {
+        this.applicationContext = applicationContext
+    }
+
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
         val packageName = "tw.waterballsa.utopia"
 
-        val reflections = org.reflections.Reflections(packageName)
-        val annotatedClasses = reflections.getTypesAnnotatedWith(Document::class.java)
-        val mongoTemplate = applicationContext.getBean(MongoTemplate::class.java)
+        val annotatedClasses = Reflections(packageName).getTypesAnnotatedWith(Document::class.java)
 
         for (annotatedClass in annotatedClasses) {
             val idField = annotatedClass.declaredFields
-                .first { it.isAnnotationPresent(Id::class.java) }!!
+                    .find { it.isAnnotationPresent(Id::class.java) }!!
             val resolvableType: ResolvableType =
-                ResolvableType.forClassWithGenerics(MongoCollection::class.java, annotatedClass, idField.type)
+                    ResolvableType.forClassWithGenerics(MongoCollection::class.java, annotatedClass, idField.type)
             val beanDefinition = RootBeanDefinition()
             beanDefinition.setTargetType(resolvableType)
             beanDefinition.autowireMode = AbstractBeanDefinition.AUTOWIRE_BY_TYPE
@@ -92,12 +97,11 @@ open class MyBeanFactoryPostProcessor : BeanFactoryPostProcessor, ApplicationCon
 
             val bf: DefaultListableBeanFactory = beanFactory as DefaultListableBeanFactory
             val collectionName = annotatedClass.kotlin.findAnnotation<Document>()?.collection
-                ?.ifEmpty { annotatedClass.simpleName }!!
+                    ?.ifEmpty { annotatedClass.simpleName }!!
 
-            val mongoCollection = MongoCollectionAdapter(
-                mongoTemplate,
-                MappingMongoDocumentInformation(collectionName, annotatedClass, idField.type, idField.name)
-            )
+            val mongoTemplate = applicationContext.getBean(MongoTemplate::class.java)
+            val documentInformation = MappingMongoDocumentInformation(collectionName, annotatedClass, idField.type, idField.name)
+            val mongoCollection = MongoCollectionAdapter(mongoTemplate, documentInformation)
 
             val beanName = "${annotatedClass.simpleName}MongoCollection"
             bf.registerBeanDefinition(beanName, beanDefinition)
@@ -105,7 +109,4 @@ open class MyBeanFactoryPostProcessor : BeanFactoryPostProcessor, ApplicationCon
         }
     }
 
-    override fun setApplicationContext(applicationContext: ApplicationContext) {
-        this.applicationContext = applicationContext
-    }
 }
