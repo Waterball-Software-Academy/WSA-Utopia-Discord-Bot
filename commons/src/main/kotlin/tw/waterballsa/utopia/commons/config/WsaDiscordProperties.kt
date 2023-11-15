9a7e9a3e209f43ec7ea4814773192ce1e968505e
Wayne Tsou <wayne60406@gmail.com>
2023-11-15 22:56:03 +0800

    :recycle: 用表情訂閱/解除訂閱機制
    更新最新版本

--- commons/src/main/kotlin/tw/waterballsa/utopia/commons/config/WsaDiscordProperties.kt
+++ commons/src/main/kotlin/tw/waterballsa/utopia/commons/config/WsaDiscordProperties.kt
@@ -9,62 +9,91 @@ const val ENV_PROD = "prod"
 
 
 open class WsaDiscordProperties(properties: Properties) {
-    val engineerLifeChannelId: String
-    val careerAdvancementTopicChannelId: String
-    val knowledgeKingChannelId: String
+
+    // Database Name
+    val mongoDatabase: String
+
+    // Guild ID
     val guildId: String
-    val unlockEntryMessageId: String
-    val selfIntroChannelId: String
-    val wsaGuestRoleId: String
-    val wsaCitizenRoleId: String
-    val gentlemenForumCategoryId: String
+
+    // Channel ID
     val wsaGentlemenBroadcastChannelId: String
     val wsaPartyChannelId: String
     val wsaGaaSConversationChannelId: String
-    val wsaGaaSMemberRoleId: String
-    val wsaAlphaRoleId: String
-    val discussionAreaChannelId: String
+    val knowledgeKingChannelId: String
+    val careerAdvancementTopicChannelId: String
+    val engineerLifeChannelId: String
     val unlockEntryChannelId: String
+    val selfIntroChannelId: String
+    val discussionAreaChannelId: String
     val flagPostChannelId: String
-    val mongoDatabase: String
     val resumeCheckChannelId: String
     val featuredVideosChannelId: String
-    val flagPostGuideId: String
-    val waterBallJournalPostId: String
-    val waterBallLoseWeightPostId: String
     val wsaGuideLineChannelId: String
+
+    // Role ID
+    val wsaGuestRoleId: String
+    val wsaCitizenRoleId: String
+    val wsaAlphaRoleId: String
+    val wsaGaaSMemberRoleId: String
     val wsaLongArticleRoleId: String
     val wsaTopicMasterRoleId: String
+
+    // Message ID
+    val unlockEntryMessageId: String
+
+    // Forum ID
+    val flagPostGuideId: String
+    val gentlemenForumCategoryId: String
+
+    // Post ID
+    val waterBallJournalPostId: String
+    val waterBallLoseWeightPostId: String
 
     init {
         properties.run {
             logger.info { properties }
+
+            // Database Name
+            mongoDatabase = getProperty("mongo-database")
+
+            // Guild ID
             guildId = getProperty("guild-id")
-            unlockEntryMessageId = getProperty("unlock-entry-message-id")
-            selfIntroChannelId = getProperty("self-intro-channel-id")
-            wsaGuestRoleId = getProperty("wsa-guest-role-id")
-            wsaCitizenRoleId = getProperty("wsa-citizen-role-id")
-            gentlemenForumCategoryId = getProperty("wsa-gentlemen-forum-category-id")
+
+            // Channel ID
             wsaGentlemenBroadcastChannelId = getProperty("wsa-gentlemen-broadcast-channel-id")
             wsaPartyChannelId = getProperty("wsa-party-channel-id")
             wsaGaaSConversationChannelId = getProperty("wsa-gaas-conversation-channel-id")
-            wsaGaaSMemberRoleId = getProperty("wsa-gaas-member-role-id")
-            wsaAlphaRoleId = getProperty("wsa-alpha-role-id")
             knowledgeKingChannelId = getProperty("knowledge-king-channel-id")
-            discussionAreaChannelId = getProperty("wsa-discussion-area-channel-id")
-            unlockEntryChannelId = getProperty("unlock-entry-channel-id")
-            flagPostChannelId = getProperty("wsa-flag-post-channel-id")
             careerAdvancementTopicChannelId = getProperty("career-advancement-topic-channel-id")
             engineerLifeChannelId = getProperty("engineer-life-channel-id")
-            mongoDatabase = getProperty("mongo-database")
+            unlockEntryChannelId = getProperty("unlock-entry-channel-id")
+            selfIntroChannelId = getProperty("self-intro-channel-id")
+            discussionAreaChannelId = getProperty("wsa-discussion-area-channel-id")
+            flagPostChannelId = getProperty("wsa-flag-post-channel-id")
             resumeCheckChannelId = getProperty("resume-check-channel-id")
             featuredVideosChannelId = getProperty("featured-videos-channel-id")
-            flagPostGuideId = getProperty("flag-post-guide-id")
-            waterBallJournalPostId = getProperty("water-ball-journal-post-id")
-            waterBallLoseWeightPostId = getProperty("water-ball-lose-weight-post-id")
             wsaGuideLineChannelId = getProperty("wsa-guideline-channel-id")
+
+            // Role ID
+            wsaGuestRoleId = getProperty("wsa-guest-role-id")
+            wsaCitizenRoleId = getProperty("wsa-citizen-role-id")
+            wsaAlphaRoleId = getProperty("wsa-alpha-role-id")
+            wsaGaaSMemberRoleId = getProperty("wsa-gaas-member-role-id")
             wsaLongArticleRoleId = getProperty("wsa-long-article-role-id")
             wsaTopicMasterRoleId = getProperty("wsa-topic-master-role-id")
+
+            // Message ID
+            unlockEntryMessageId = getProperty("unlock-entry-message-id")
+
+            // Forum ID
+            flagPostGuideId = getProperty("flag-post-guide-id")
+            gentlemenForumCategoryId = getProperty("wsa-gentlemen-forum-category-id")
+
+            // Post ID
+            waterBallJournalPostId = getProperty("water-ball-journal-post-id")
+            waterBallLoseWeightPostId = getProperty("water-ball-lose-weight-post-id")
         }
     }
 }
+
