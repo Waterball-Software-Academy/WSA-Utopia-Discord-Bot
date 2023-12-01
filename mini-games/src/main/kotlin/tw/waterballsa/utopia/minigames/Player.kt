cd8aa60e169e6901b44a6c3ff6315282ba20f204
frankvicky <chi972121@gmail.com>
2023-12-01 22:22:27 +0800

    :building_construction: 小遊戲模組外殼

---
+++ mini-games/src/main/kotlin/tw/waterballsa/utopia/minigames/Player.kt
@@ -0,0 +1,6 @@
+package tw.waterballsa.utopia.minigames
+
+data class Player(
+    val id: String
+// TODO: add required fields
+)
---
+++ mini-games/src/main/kotlin/tw/waterballsa/utopia/minigames/PlayerFinder.kt
@@ -0,0 +1,6 @@
+package tw.waterballsa.utopia.minigames
+
+interface PlayerFinder {
+    fun findById(id: String): Player
+}
+// TODO: implement interface
