76a43ce2a45f23b9cfb1eed5f7d5464cf68e4ba7
michael <miiichaelee@gmail.com>
2024-02-18 14:52:11 +0800

    fix 程式寫法

--- top-post/src/main/kotlin/tw/waterballsa/utopia/TopPostListener.kt
+++ top-post/src/main/kotlin/tw/waterballsa/utopia/TopPostListener.kt
@@ -1,24 +1,31 @@
 package tw.waterballsa.utopia.toppost
 
+import net.dv8tion.jda.api.entities.channel.unions.IThreadContainerUnion
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent
 import org.springframework.stereotype.Component
 import tw.waterballsa.utopia.commons.config.WsaDiscordProperties
 import tw.waterballsa.utopia.jda.UtopiaListener
 
-const val goodEmoji = "\uD83D\uDC4D"
+const val likeEmoji = "\uD83D\uDC4D"
 
 
 @Component
 class TopPostListener(private val wsa: WsaDiscordProperties) : UtopiaListener() {
 
     override fun onMessageReactionAdd(event: MessageReactionAddEvent) {
-        val channel = event.channel
-        val parentChannel = channel.asThreadChannel().parentChannel
-        if (wsa.topicPondChannelId == parentChannel.id && goodEmoji == (event.emoji.name)) {
-            channel.sendMessage(goodEmoji).queue {
-                it.delete().queue()
+        with (event) {
+            val post = channel.asThreadChannel()
+            val forum = post.parentChannel
+            val hasPostLikeEmoji = emoji.name == likeEmoji
+
+            if (forum.isTopicPoolForum() && hasPostLikeEmoji) {
+                channel.sendMessage(likeEmoji).queue {
+                    it.delete().queue()
+                }
             }
         }
     }
+
+    private fun IThreadContainerUnion.isTopicPoolForum() = id == wsa.topicPondChannelId
 
 }
